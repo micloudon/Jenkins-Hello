d45fb44cda337f3fdc85444af790e9dda64fb5f7
Michael Loudon <micloudon@gmail.com>
2021-05-19 11:12:24 -0700

    init commit

---
+++ src/com/jenkins_hello/HelloJenkins.java
@@ -0,0 +1,11 @@
+package com.jenkins_hello;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("Hello From Jenkins");
+		System.out.println("Hello again From Jenkins");
+	}
+	
+
+}
